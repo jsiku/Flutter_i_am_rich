0f9363a4b6d0077907e97aa61eb6a8d9d7bfa202
jsiku <j.siku.cho@gmail.com>
2022-06-24 05:08:49 +0200

    networkimage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
-        ),
+      MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey[900],
+            appBar: AppBar(
+              title: const Text('I Am Rich'),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: const Image(
+              image: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png'),
+            )),
       ),
     );
