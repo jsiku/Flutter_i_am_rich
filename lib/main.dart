7197a3e99cd922512f5a979c5bc0a15cc7284baf
jsiku <j.siku.cho@gmail.com>
2022-06-24 05:49:14 +0200

    without Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.blueGrey[900],
-            appBar: AppBar(
-              title: const Text('I Am Rich'),
-              backgroundColor: Colors.blueGrey[900],
-            ),
-            body: const Image(
-              image: NetworkImage(
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png'),
-            )),
+//The main function is the entrance point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
-    );
+    ),
+  );
+}
